729a218569814365e82d19c16e4825e03468898a
alejandroP03 <d.pulidob@uniandes.edu.co>
2026-07-21 21:58:32 -0500

    Cambio chiper->cheapest

--- scripts/seed-database.ts
+++ scripts/seed-database.ts
@@ -35,7 +35,7 @@ process.env.DB_HOST ??= '127.0.0.1';
 process.env.DB_PORT ??= '5432';
 process.env.DB_USERNAME ??= 'postgres';
 process.env.DB_PASSWORD ??= 'postgres';
-process.env.DB_NAME ??= 'chiper';
+process.env.DB_NAME ??= 'cheapest';
 
 async function initializeDataSourceWithRetry(entities: Function[]) {
   let lastError: unknown;
